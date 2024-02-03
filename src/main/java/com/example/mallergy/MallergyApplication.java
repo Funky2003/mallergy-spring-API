cad37bcabd788ebf7a8ddadf044810489da0756e
Funky2003 <nusetorsetsofia101@gmail.com>
2024-02-03 18:17:54 +0000

    Mallergy API init

---
+++ src/main/java/com/example/mallergy/MallergyApplication.java
@@ -0,0 +1,13 @@
+package com.example.mallergy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallergyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallergyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mallergy/MallergyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mallergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallergyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
