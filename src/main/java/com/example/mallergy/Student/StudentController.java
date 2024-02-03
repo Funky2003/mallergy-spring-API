89ad23ec124c8307e2de5ea5667f06b528adad7f
Funky2003 <nusetorsetsofia101@gmail.com>
2024-02-03 19:26:16 +0000

    Network test

--- src/main/java/com/example/mallergy/Student/StudentController.java
+++ src/main/java/com/example/mallergy/Student/StudentController.java
@@ -1,9 +1,11 @@
 package com.example.mallergy.Student;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/api/v1/student")
 public class StudentController {
 
     @GetMapping
