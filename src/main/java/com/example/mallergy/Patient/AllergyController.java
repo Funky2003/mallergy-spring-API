a80d741096c0e72c7a8c2c2b52e46429b6b0b481
Funky2003 <nusetorsetsofia101@gmail.com>
2024-02-03 23:35:14 +0000

    Updated AllergyController and AllergyService

--- src/main/java/com/example/mallergy/Patient/AllergyController.java
+++ src/main/java/com/example/mallergy/Patient/AllergyController.java
@@ -1,11 +1,27 @@
 package com.example.mallergy.Patient;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/v1/allergy")
 public class AllergyController {
 
+    //<--Initialize the allergy services and with the constructor
+    //<--Also Autowire the constructor
+    private final AllergyService allergyService;
+    @Autowired
+    public AllergyController(AllergyService allergyService) {
+        this.allergyService = allergyService;
+    }
+
+    //<--Create the action to get all allergies with a @GetMapping annotation
+    @GetMapping
+    public List<AllergyModel> getAllergy() {
+        return allergyService.getAllAllergyRecords();
+    }
 }
--- src/main/java/com/example/mallergy/Patient/AllergyService.java
+++ src/main/java/com/example/mallergy/Patient/AllergyService.java
@@ -3,6 +3,8 @@ package com.example.mallergy.Patient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class AllergyService {
     //<--To start, first initialize the Allergy repository
@@ -11,5 +13,10 @@ public class AllergyService {
     @Autowired
     public AllergyService(AllergyRepository allergyRepository) {
         this.allergyRepository = allergyRepository;
+    }
+
+    //<--Method to get all the allergy records
+    public List<AllergyModel> getAllAllergyRecords() {
+        return allergyRepository.findAll();
     }
 }
